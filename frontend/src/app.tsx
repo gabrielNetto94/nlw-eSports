f57955cfae38327652ea582fc63565b05a452d70
gabrielNetto94 <silveiranetto94@gmail.com>
2022-09-17 12:55:58 -0300

    estilização inicial frontend

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,12 +1,27 @@
-function App() {
+import "./styles/main.css";
+import logoImg from "./assets/logo-nlw-esports.png";
 
+function App() {
   return (
-    <>
-    <div className="w-8 h-8 bg-black">
-      asdas
+
+    <div className="max-w-[1344] mx-auto flex items-center flex-col my-20">
+      <img src={logoImg} />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          {" "}
+          duo{" "}
+        </span>
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6"></div>
+        <a href="" className="">
+
+        </a>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
