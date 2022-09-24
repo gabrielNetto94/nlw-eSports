deee0e27ca43f5e0c1328fd53e844d09348e6600
gabrielNetto94 <silveiranetto94@gmail.com>
2022-09-24 15:40:07 -0300

    (backend) conexao com banco e rota para games e ads

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Next level week - Rocketseat
 Projeto frontend disponível em:
 
 https://nlw-e-sports-lovat.vercel.app/
+
+
+Backend
+
+    npx prisma migrate dev
+
+    npx prisma studio
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,25 @@
-const express = require('express')
+import express, {Request, Response} from 'express'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 
-app.get('/', (req:any, res:any) => {
-    res.send({
-        teste1: 'asdas',
-        teste4: 'asdas',
+app.get('/games', async (req: Request, res: Response) => {
 
-    })
+    const games = await prisma.game.findMany()
+
+    return res.json(games)
 })
 
-app.listen(3333,()=>{
+app.get('/ads', async (req: Request, res: Response) => {
+
+    const ads = await prisma.ad.findMany()
+
+    return res.json(ads)
+
+})
+
+app.listen(3333, () => {
     console.log(`server rodando na porta 3333`)
 })
